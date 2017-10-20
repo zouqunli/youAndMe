7de24232925cba5409b192f968f26aacfbd9911c
qlzou <zouqunli122@qq.com>
2017-10-20 14:25:31 +0800

    test

--- app/src/main/java/com/mdm/youandme/MainActivity.java
+++ app/src/main/java/com/mdm/youandme/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 	private int i = 0;
-	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
