ded6dc00f06091df6e9ff4729cbbc36b178b1059
qlzou <zouqunli122@qq.com>
2017-10-20 12:48:03 +0800

    测试

--- app/src/main/java/com/mdm/youandme/MainActivity.java
+++ app/src/main/java/com/mdm/youandme/MainActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+	private int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
 }
